55cf34ac8354cd3def0c63801a1a70446aaef702
vavaliyasonal1311 <vavaliyasonal@gmail.com>
2025-07-11 12:18:31 +0530

    Updated CompactReviewCardView.tsx

--- src/components/CompactReviewCardView.tsx
+++ src/components/CompactReviewCardView.tsx
@@ -120,7 +120,7 @@ export const CompactReviewCardView: React.FC<CompactReviewCardViewProps> = ({ ca
                       <img
                         src="/yashv.jpg"
                         alt="YP Logo"
-                        className="w-20 h-20 sm:w-12 sm:h-12 lg:w-10 lg:h-10 object-contain rounded-xl  border border-blue"
+                        className="w-20 h-20 sm:w-12 sm:h-12 lg:w-10 lg:h-10 object-contain rounded-xl  border-2 border-blue"
                       />         
           
           {/* Star Rating Selector */}
