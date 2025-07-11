2031667b6223c302973df62f229afbb74802e051
vavaliyasonal1311 <vavaliyasonal@gmail.com>
2025-07-11 12:11:44 +0530

    Updated AdminDashboard.tsx

--- src/components/AdminDashboard.tsx
+++ src/components/AdminDashboard.tsx
@@ -249,7 +249,7 @@ export const AdminDashboard: React.FC = () => {
                       <img
                         src="/yashv.jpg"
                         alt="YP Logo"
-                        className="w-20 h-20 sm:w-12 sm:h-12 lg:w-14 lg:h-14 object-contain rounded-xl  border border-white"
+                        className="w-20 h-20 sm:w-12 sm:h-12 lg:w-24 lg:h-24 object-contain rounded-xl  border border-white"
                       />
                   </div>     
             Review Cards Dashboard
