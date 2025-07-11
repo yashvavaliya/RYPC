6bac57bfa4a4efebc35fd8fbf81b71597b40b3ec
vavaliyasonal1311 <vavaliyasonal@gmail.com>
2025-07-11 14:04:05 +0530

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -95,7 +95,7 @@ const DynamicReviewCard: React.FC = () => {
             Contect Admin : +91 12345 6789
           </p>
           <a
-            href="/LoginPage"
+            href="/admin"
             className="inline-flex items-center px-6 py-3 bg-gradient-to-r from-blue-600 to-purple-600 text-white font-semibold rounded-xl hover:from-blue-700 hover:to-purple-700 transition-all duration-300"
           >
             Go to Dashboard
