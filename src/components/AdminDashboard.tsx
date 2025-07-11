bf954102bb22f86f514877c6af1b28a4a1a2b2bb
vavaliyasonal1311 <vavaliyasonal@gmail.com>
2025-07-11 12:08:21 +0530

    Updated AdminDashboard.tsx

--- src/components/AdminDashboard.tsx
+++ src/components/AdminDashboard.tsx
@@ -245,16 +245,13 @@ export const AdminDashboard: React.FC = () => {
           
           <h1 className="text-4xl lg:text-5xl font-bold text-white mb-4 bg-gradient-to-r from-blue-400 via-purple-400 to-pink-400 bg-clip-text text-transparent">
 
-  <div className=" flex items-center justify-center mb-8">            
+  <div className=" flex items-center justify-center mb-8 rounded-xl p-6 border border-white/20">            
                       <img
                         src="/yashv.jpg"
                         alt="YP Logo"
                         className="w-10 h-10 sm:w-12 sm:h-12 lg:w-14 lg:h-14 animate-pulse-gentle transform-gpu object-contain"
                       />
-                  </div>
-         
-
-            
+                  </div>     
             Review Cards Dashboard
           </h1>
           <p className="text-slate-300">
